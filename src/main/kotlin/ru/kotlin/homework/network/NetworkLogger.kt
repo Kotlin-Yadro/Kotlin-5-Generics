05440d188f8e4280fe213b92242af8acdb20dbcb
Nikita Krasnoyarov <n.krasnoyarov@yadro.com>
2024-09-18 13:43:14 +0300

    Homework 2.2 - Nikita Krasnoyarov

--- src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
+++ src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
@@ -15,24 +15,37 @@ sealed class ApiException(message: String) : Throwable(message) {
     data object UnknownException: ApiException("Unknown exception")
 }
 
-class ErrorLogger<E : Throwable> {
+interface DumpInterface<out E> {
+    fun dump(): List<Pair<LocalDateTime, E>>
+}
 
-    val errors = mutableListOf<Pair<LocalDateTime, E>>()
+interface LogInterface<in E> {
+    fun log(response: NetworkResponse<*, E>)
+    fun dumpLog()
+}
 
-    fun log(response: NetworkResponse<*, E>) {
+class ErrorLogger<E : Throwable> : DumpInterface<E>, LogInterface<E> {
+
+    private val errors = mutableListOf<Pair<LocalDateTime, E>>()
+
+    override fun log(response: NetworkResponse<*, E>) {
         if (response is Failure) {
             errors.add(response.responseDateTime to response.error)
         }
     }
 
-    fun dumpLog() {
+    override fun dumpLog() {
         errors.forEach { (date, error) ->
             println("Error at $date: ${error.message}")
         }
     }
+
+    override fun dump(): List<Pair<LocalDateTime, E>> {
+        return errors
+    }
 }
 
-fun processThrowables(logger: ErrorLogger<Throwable>) {
+fun processThrowables(logger: LogInterface<Throwable>) {
     logger.log(Success("Success"))
     Thread.sleep(100)
     logger.log(Success(Circle))
@@ -42,7 +55,7 @@ fun processThrowables(logger: ErrorLogger<Throwable>) {
     logger.dumpLog()
 }
 
-fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
+fun processApiErrors(apiExceptionLogger: LogInterface<ApiException>) {
     apiExceptionLogger.log(Success("Success"))
     Thread.sleep(100)
     apiExceptionLogger.log(Success(Circle))
@@ -52,13 +65,22 @@ fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
     apiExceptionLogger.dumpLog()
 }
 
+fun dumpErrors(logger: DumpInterface<*>) {
+    println("Dumping using dump interface:")
+    logger.dump().forEach { (date, error) ->
+        println("Error at $date: ${(error as Throwable).message}")
+    }
+}
+
 fun main() {
     val logger = ErrorLogger<Throwable>()
 
     println("Processing Throwable:")
     processThrowables(logger)
+    dumpErrors(logger)
 
     println("Processing Api:")
     processApiErrors(logger)
+    dumpErrors(logger)
 }
 
