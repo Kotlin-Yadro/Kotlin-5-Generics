7607741c6843d4c4966b170a5c884a2c8fcc2a64
davydov.boris@tvkvant.ru <mind.shade@gmail.com>
2024-09-19 15:44:24 +0300

    ДЗ #2 Generics
    -- Исправьте определение классов так, чтобы все присваивания под определениями компилировались без ошибок.
    -- Почините (правильно расставьте variance параметров) класс NetworkLogger таким образом, чтобы один универсальный экземпляр логгера можно было использовать для логирования любых ошибок
    -- Сделайте так, чтобы NetworkLogger имел возможность выдать список накопленных ошибок.

--- src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
+++ src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
@@ -15,9 +15,9 @@ sealed class ApiException(message: String) : Throwable(message) {
     data object UnknownException: ApiException("Unknown exception")
 }
 
-class ErrorLogger< in E : Throwable> {
+class ErrorLogger<E : Throwable> {
 
-    val errors: MutableList<Pair<LocalDateTime, @UnsafeVariance E>> = mutableListOf()
+    private val errors: MutableList<Pair<LocalDateTime, E>> = mutableListOf()
 
     fun log(response: NetworkResponse<*, E>) {
         if (response is Failure) {
@@ -31,7 +31,7 @@ class ErrorLogger< in E : Throwable> {
         }
     }
 
-    fun dump(): List<Pair<LocalDateTime, @UnsafeVariance E>> {
+    fun dump(): List<Pair<LocalDateTime, E>> {
         return errors
     }
 }
@@ -42,11 +42,13 @@ fun processThrowables(logger: ErrorLogger<Throwable>) {
     logger.log(Success(Circle))
     Thread.sleep(100)
     logger.log(Failure(IllegalArgumentException("Something unexpected")))
-
     logger.dumpLog()
+
+    val errors = logger.dump()
+    println("Errors: $errors")
 }
 
-fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
+fun processApiErrors(apiExceptionLogger: ErrorLogger<in ApiException>) {
     apiExceptionLogger.log(Success("Success"))
     Thread.sleep(100)
     apiExceptionLogger.log(Success(Circle))
@@ -54,6 +56,9 @@ fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
     apiExceptionLogger.log(Failure(ApiException.NetworkException))
 
     apiExceptionLogger.dumpLog()
+
+    val errors = apiExceptionLogger.dump()
+    println("Errors: $errors")
 }
 
 fun main() {
